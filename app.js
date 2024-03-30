9973d445642ce1d566dba60f267ec8a8c622ea4d
Muhammad <anasfaraz@gmail.com>
2024-03-30 11:52:01 +0500

    JS-class-08 code

---
+++ app.js
@@ -0,0 +1,121 @@
+// var Newyeardate = new Date("1 january 2025").getTime();
+// var currnetDate = new Date().getTime();
+
+// var diff = Newyeardate - currnetDate
+
+// var month = diff / (1000 * 60 * 60 * 24 * 30);
+// var days = diff / (1000 * 60 * 60 * 24);
+// var hour = diff / (1000 * 60 * 60);
+// var minute = diff / (1000 * 60);
+// var second = diff / (1000);
+
+// var month = Math.floor(month);
+// var days = Math.floor(days);
+// var hour = Math.floor(hour);
+// var minute = Math.floor(minute);
+// var second = Math.floor(second);
+
+// console.log("Month remainig new year === ",month);
+// console.log("Days", days, " Hr", hour, " Min", minute, " Sec", second);
+
+                                 //  Switch-Statement 
+// var userinput = prompt("Enter your Gender ");
+// var userinputs = userinput.toLowerCase();
+// switch (userinput) {
+//     case "male":
+//         alert("HELLO! Boy what's your name");
+//         break;
+//     case "girl":
+//         alert("HELLO! Girl what's your name");
+//         break;
+//     default:
+//         alert("Not found...")
+// }
+
+
+                                        // TABLE FOR WHILE_LOOP
+var userinput = +prompt("Enter your table number");
+// var table=  userinput * 1
+var i = 0
+while(i <= 10){
+ document.write(userinput + "<br>");
+i
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
